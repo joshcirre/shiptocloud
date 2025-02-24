14d412fe8c80b9c93fc5e1f1824f1efb92d5bebe
Josh Cirre <joshcirre@gmail.com>
2025-02-24 13:34:54 -0700

    new branch update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app>
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <p>Hi there YouTube!</p>
+            <p>Hi there YouTube, this is a completely different branch!</p>
         </div>
         <div class="relative h-full flex-1 rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
